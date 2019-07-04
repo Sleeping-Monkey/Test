c3111556bf8ea8081e45397a8140164ce2034a77
root <root@debian.21-school.ru>
2019-07-04 10:43:28 +0300

    writing every min

--- md1.c
+++ md1.c
@@ -3,9 +3,42 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/timer.h>
+#include <linux/ktime.h>
+#include <linux/jiffies.h>
  
 MODULE_LICENSE("GPL"); 
 MODULE_AUTHOR("Appa"); 
+
+struct timer_list my_timer;
+
+void	my_func(struct timer_list *data)
+{
+	struct file		*file = NULL;
+	ssize_t			n = 0;
+	loff_t			offset = 0;
+	mm_segment_t	fs;
+
+	printk("[!] my_funk start\n");
+	file = filp_open("/root/Test2/test2.txt", O_WRONLY|O_APPEND, 0644);
+	if (IS_ERR(file))
+	{
+		printk("[!] file open failed\n");
+		return ;
+	}
+	fs = get_fs();
+	set_fs(get_ds());
+	if ((n = vfs_write(file, "Dub\n", 4, &offset)) != 4)
+	{
+		printk("[!] failed write: %ld\n", n);
+		filp_close(file, NULL);
+		return ;
+	}
+	printk("[!] writing is ok\n");
+	set_fs(fs);
+	filp_close(file, NULL);
+	mod_timer(&my_timer, jiffies + 60 * HZ);
+}
 
 static int __init md_init(void)
 {
@@ -15,7 +48,7 @@ static int __init md_init(void)
 	loff_t			offset = 0;
 
 	printk("[!] module md1 start!\n");
-	file = filp_open("test.txt", O_WRONLY|O_CREAT, 0644);
+	file = filp_open("test2.txt", O_WRONLY|O_CREAT|O_APPEND, 0644);
 	if (IS_ERR(file))
 	{
 		printk("[!] file open failed\n");
@@ -32,10 +65,16 @@ static int __init md_init(void)
 	printk("[!] write %ld bytes\n", n);
 	filp_close(file, NULL);
 	set_fs(fs);
+//	init_timers();
+	my_timer.expires = jiffies + 10 * HZ;
+//	my_timer.data = 0;
+	my_timer.function = my_func;
+	add_timer(&my_timer);
 	return 0;
 }
 
 static void __exit md_exit(void) {
+	del_timer(&my_timer);
 	printk("[!] module md1 unloaded!\n"); 
 }
 
